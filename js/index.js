4e5b024d4f4079aadb0d4a02e5dbeba826a795eb
Marat Valiev <mz.valiev@gmail.com>
2018-05-07 23:44:40 -0400

    Add new compact gallery

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 const gridGallery = $('#grid-gallery-selector');
 const cardsGallery = $('#cards-gallery-selector');
+const compactGallery = $('#compact-gallery-selector');
 const gridGalleryBox = $('.grid-gallery-box');
 const cardsGalleryBox = $('.cards-gallery-box');
+const compactGalleryBox = $('.compact-gallery-box');
 
 
 cardsGallery.click(() => {
-    gridGalleryBox.fadeOut(500);
     cardsGalleryBox.fadeIn(500);
+    gridGalleryBox.fadeOut(500);
+    compactGalleryBox.fadeOut(500);
 })
 
 gridGallery.click(() => {
     gridGalleryBox.fadeIn(500);
+    compactGalleryBox.fadeOut(500);
+    cardsGalleryBox.fadeOut(500);
+})
+
+compactGallery.click(() => {
+    compactGalleryBox.fadeIn(500);
+    gridGalleryBox.fadeOut(500);
     cardsGalleryBox.fadeOut(500);
 })
